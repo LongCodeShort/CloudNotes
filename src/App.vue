d14120ddae1f52f04cd1b8e10e9d5376368acdaa
chasec <852533969@qq.com>
2021-06-17 17:08:42 +0800

    添加 element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <span>测试</span>
-  </div>
   <router-view/>
 </template>
 
