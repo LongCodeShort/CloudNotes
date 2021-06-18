b1199cca58316e4d25f226e29efb16d481c73898
chasec <852533969@qq.com>
2021-06-18 18:01:34 +0800

    添加 home 背景

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="home">
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <div>
+
+    </div>
   </div>
 </template>
 
@@ -14,3 +12,12 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    width: 100vw;
+    height: 100vh;
+    background-image: url('../assets/gif/note.gif');
+    background-size: cover;
+  }
+</style>
